f6d20c704469c28192d87c672b37c803f10e9c9b
Eduardo Almeida <megaedux@me.com>
2015-02-28 15:07:39 +0000

    More stubs.

--- src/pt/up/fe/Messaging/Message.java
+++ src/pt/up/fe/Messaging/Message.java
@@ -7,4 +7,16 @@ import java.net.InetAddress;
  */
 public class Message {
     InetAddress source;     //  This may not be used after all.
+
+    void makeHeader() {
+        /*
+         *      This doesn't return void of course, it's just a stub for now.
+         */
+    }
+
+    void makeMessage(String header, String body) {
+        /*
+         *      This doesn't return void of course, it's just a stub for now... Neither has string as input arguments.
+         */
+    }
 }
